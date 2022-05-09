2910d327c430c0e32974876a711f03557b4b4e5e
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-05-09 14:10:02 +0300

    if else

---
+++ main.py
@@ -0,0 +1,16 @@
+a = float(input('First: '))
+b = float(input('Second: '))
+op = input('Operation (sum/sub/div/mul): ')
+
+if op== 'sum':
+    print(f'a+b = {a+b}')
+elif op=='div':
+    print(f'a+b = {a+b}')
+elif op =='sub':
+    print(f'a+b = {a+b}')
+elif op=='mul':
+    print(f'a+b = {a+b}')
+else:
+    print("Invalid Choice")
+    
+
