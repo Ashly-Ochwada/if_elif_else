aec639433afd5fbed3b6437fa6aec51dcf346e69
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-05-10 09:42:32 +0300

    def

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def sum(a,b):
+    return a+b
+
+def div(a,b):
+    return a/b
+
+def sub(a,b):
+    return a-b  
+
+def mul(a,b):
+    return a*b          
+
 a = float(input('First: '))
 b = float(input('Second: '))
 op = input('Operation (sum/sub/div/mul): ')
@@ -14,3 +26,12 @@ else:
     print("Invalid Choice")
     
 
+#sentence = "the quick brown fox jumped over the lazy dog"
+#record = {}
+
+#for letter in sentence:
+ #   if letter in record:
+  #      record[letter] +=1
+   # else:
+    #    record[letter] =1
+#print (record)           
